3185ed1a89642479b3adc4822b54c0fd73e3127a
Abhijit Vaidya <abhijit@nclouds.com>
2013-07-25 16:34:45 +0530

    adding apache and mysql

---
+++ elinks/metadata.rb
@@ -0,0 +1,7 @@
+name             'elinks'
+maintainer       'mph'
+maintainer_email 'abhijit@nlcouds.com'
+license          'All rights reserved'
+description      'Installs/Configures elinks'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ elinks/recipes/apache.rb
@@ -0,0 +1,1 @@
+#adding apache to server
